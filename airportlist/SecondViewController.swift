f13c618cdb0714a850267cd67f26c608ef3b2134
kkmao <bra224max852zxc@gmail.com>
2017-05-30 23:52:07 +0800

    push

---
+++ airportlist/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  airportlist
+//
+//  Created by 李紹威 on 2017/5/30.
+//  Copyright © 2017年 毛維辰. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
